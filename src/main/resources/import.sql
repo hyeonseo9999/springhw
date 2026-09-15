6fc6c35d2b2870531ed16c6be5c7a10e951f49c8
hyeonseo9999 <gustj5161@gmail.com>
2026-09-15 19:43:26 +0900

    Fix:하반기 2주차 과제

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,2 @@
-INSERT INTO guestbooks (title, content, writer, created_at, ps)
-VALUES ('방명록 남겨요!', '2주차 JPA 세션 완강했습니다.', '아기사자', NOW(), '다음 주 CRUD도 기대됩니다.');
-
-INSERT INTO guestbooks (title, content, writer, created_at, ps)
-VALUES ('안녕하세요', '스프링 부트 재밌네요.', '멋쟁이사자', NOW(), NULL);
+INSERT INTO guestbooks (title, content, writer, created_at, ps) VALUES ('test1', 'test1', '권현서', NOW(), 'test1');
+INSERT INTO guestbooks (title, content, writer, created_at, ps) VALUES ('test2', 'test2', '권현서', NOW(), 'test2');
